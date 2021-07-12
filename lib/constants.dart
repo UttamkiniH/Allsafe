ac496a7afae5b1a0ecedab1771eb2f337500dc26
UttamkiniH <huttamkini23@gmail.com>
2021-07-12 20:13:56 +0530

    added constant.dart

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+const  headlineMain = TextStyle(
+  color: primaryColor,
+  fontFamily: 'Public Sans',
+  fontSize: 30,
+);
+
+const primaryColor = Color.fromRGBO(56, 57, 128, 1);
+const secondaryColor = Colors.white;
+const cardColor = Color.fromRGBO(50, 51, 128, 1);
+const accentColor = Color.fromRGBO(44, 46, 135, 1);
---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+// not needed right now i guess
+
+// import 'package:allsafe/constants.dart';
+// import 'package:flutter/material.dart';
+
+// ThemeData appTheme() {
+//   TextTheme _appTextTheme(TextTheme base) {
+//     return base.copyWith(
+//       headline5: base.headline5.copyWith(
+//         fontFamily: 'Public Sans',
+//         // color: primaryColor,
+//         fontSize: 30,
+//       ),
+//     );
+//   }
+
+//   final ThemeData base = ThemeData.light();
+//   return base.copyWith(
+//     textTheme: _appTextTheme(base.textTheme),
+//     primaryColor: Color.fromRGBO(56, 57, 128, 1),
+//     // accentColor:
+//   );
+// }
